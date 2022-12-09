78e7b377bce9689fdcd71787a22404a343b13cdf
NeptuneRjo <rjo9000@outlook.com>
2022-12-09 16:28:23 -0500

    initialize django rest framework

--- backend/ecommerce/ecommerce/settings.py
+++ backend/ecommerce/ecommerce/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # Use Django's standard 'django.contrib.auth' permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': {
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    }
+
+}
