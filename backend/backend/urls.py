137d2cc49c16024cf4118b84fa0c2dba6ff724cf
NeptuneRjo <rjo9000@outlook.com>
2022-12-09 19:41:49 -0500

    reinitialize to make backend the project folder for the django app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""ecommerce URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from ecommerce_api import urls as ecommerce_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api', include(ecommerce_urls)),
 ]
