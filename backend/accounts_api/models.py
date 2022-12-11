c164fd5e3e604b150371f268ff492b801066cff1
NeptuneRjo <rjo9000@outlook.com>
2022-12-11 02:44:36 -0500

    add method to return list of cart items

--- backend/accounts_api/models.py
+++ backend/accounts_api/models.py
@@ -16,3 +16,16 @@ class Account(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     account_cart = models.ManyToManyField(StoreItem)
+
+    def __str__(self):
+        return self.user.username
+
+    # Returns a list of cart items (StoreItem)
+
+    def cart(self):
+        cart_list = []
+
+        for item in self.account_cart.all():
+            cart_list.append(item)
+
+        return cart_list
