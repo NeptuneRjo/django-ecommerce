91a8da0f53d6826fe3fd295ee879aad82f0fe5ca
NeptuneRjo <rjo9000@outlook.com>
2022-12-13 20:56:30 -0500

    cleanup imports; remove unused imports from accounts_api.views

--- backend/accounts_api/views.py
+++ backend/accounts_api/views.py
@@ -2,13 +2,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
-from rest_framework import status
 
 from ecommerce_api.models import StoreItem
-
 from .serializers import AccountSerializer, RegistrationSerializer
 from .models import Account
-from django.contrib.auth.models import User
 
 # register user
 
