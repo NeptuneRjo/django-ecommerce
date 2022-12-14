8214cdba0b9dd5478ce5c2bea388d5f86f4bcb46
NeptuneRjo <rjo9000@outlook.com>
2022-12-14 17:09:00 -0500

    setup app container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getItems } from './API'
+import { Account, StoreItem } from './types'
+
 import './App.css'
 
 function App() {
+	const [items, setItems] = useState<StoreItem[]>([])
+	const [userToken, setUserToken] = useState<string>('')
+	const [user, setUser] = useState<undefined | Account>(undefined)
+
+	useEffect(() => {
+		;(async () => {
+			const fetchedItems = await getItems()
+
+			setItems(fetchedItems)
+		})()
+	}, [])
+
 	return <div className='App'></div>
 }
 
