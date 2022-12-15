c6d1f6d535c6deb49085fc1ea708297d77e89613
NeptuneRjo <rjo9000@outlook.com>
2022-12-14 21:40:24 -0500

    create storeitem component

---
+++ frontend/src/components/StoreItem/StoreItem.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StoreItem } from '../../types'
+
+type Props = {
+	item: StoreItem
+}
+
+const StoreItem: React.FC<Props> = ({ item }: Props) => {
+	return (
+		<div className='storeitem-main'>
+			<div className='storeitem-image'>
+				<img src={item.item_thumb_url} />
+			</div>
+			<div className='storeitem-info'>
+				<p className='price'>{item.item_price}</p>
+				<button>Add to Cart</button>
+			</div>
+		</div>
+	)
+}
+
+export default StoreItem
