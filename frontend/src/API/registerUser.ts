797c6a5955cc8ddb6f2ede775acdca0a633e1b9a
NeptuneRjo <rjo9000@outlook.com>
2022-12-15 19:15:55 -0500

    add error handling for registeruser api

--- frontend/src/API/registerUser.ts
+++ frontend/src/API/registerUser.ts
@@ -7,6 +7,10 @@ export const registerUser = async (
 		'http://localhost:8000/api/accounts/register',
 		{
 			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
 			body: JSON.stringify({
 				username: username,
 				password: password,
@@ -16,5 +20,18 @@ export const registerUser = async (
 	)
 	const json = await response.json()
 
-	return json
+	const userConflict = 'A user with that username already exists.'
+
+	if (response.ok) {
+		if (json?.username[0] === userConflict) {
+			return {
+				data: undefined,
+				error: userConflict,
+			}
+		} else {
+			return { data: json, error: undefined }
+		}
+	} else {
+		return { data: undefined, error: 'Error creating account.' }
+	}
 }
