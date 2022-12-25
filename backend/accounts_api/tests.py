a501245efe0766c1ed4df7f726b8965701ddc256
NeptuneRjo <rjo9000@outlook.com>
2022-12-25 15:29:25 -0500

    model tests

--- backend/accounts_api/tests.py
+++ backend/accounts_api/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from .models import Account
+from ecommerce_api.models import StoreItem
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class AccountTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username='testUser',
+            password='testpassword'
+        )
+        Account.objects.create(user=user)
+        StoreItem.objects.create(
+            item_name='test',
+            item_price=3.99,
+            item_thumb_url='https://example.com'
+        )
+
+    def test_account_name_matches(self):
+        test = Account.objects.get(id=1)
+        self.assertEqual(test.user.username, 'testUser')
+
+    def test_account_cart_matches(self):
+        test = Account.objects.get(id=1)
+        item = StoreItem.objects.get(item_id=1)
+
+        test.account_cart.add(item)
+        cart = test.account_cart.get(item_id=1)
+
+        self.assertEqual(cart, item)
+
+    def test_account_shows_name_query(self):
+        test = Account.objects.get(id=1)
+        self.assertEqual(str(test.user.username), 'testUser')
+
+    def test_account_cart_method(self):
+        test = Account.objects.get(id=1)
+        item = StoreItem.objects.get(item_id=1)
+
+        test.account_cart.add(item)
+        cart = test.cart()
+
+        self.assertEqual(cart, [item])
--- backend/ecommerce_api/tests.py
+++ backend/ecommerce_api/tests.py
@@ -1,3 +1,25 @@
+from decimal import Decimal
 from django.test import TestCase
+from .models import StoreItem
 
-# Create your tests here.
+
+class StoreItemTestCase(TestCase):
+    def setUp(self):
+        StoreItem.objects.create(
+            item_name='test', item_price=3.99, item_thumb_url='https://example.com')
+
+    def test_item_name_matches(self):
+        test = StoreItem.objects.get(item_id=1)
+        self.assertEqual(test.item_name, 'test')
+
+    def test_item_price_matches(self):
+        test = StoreItem.objects.get(item_id=1)
+        self.assertEqual(test.item_price, Decimal('3.99'))
+
+    def test_item_url_matches(self):
+        test = StoreItem.objects.get(item_id=1)
+        self.assertEqual(test.item_thumb_url, 'https://example.com')
+
+    def test_item_matches_query_name(self):
+        test = StoreItem.objects.get(item_id=1)
+        self.assertEqual(str(test), 'test')
