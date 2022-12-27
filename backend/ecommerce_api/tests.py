4086ed00a915e73c10e2f78097924dae782bdd79
NeptuneRjo <rjo9000@outlook.com>
2022-12-26 23:32:14 -0500

    tests

--- backend/ecommerce_api/tests.py
+++ backend/ecommerce_api/tests.py
@@ -2,6 +2,11 @@ from decimal import Decimal
 from django.test import TestCase
 from .models import StoreItem
 
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+
 
 class StoreItemTestCase(TestCase):
     def setUp(self):
@@ -23,3 +28,17 @@ class StoreItemTestCase(TestCase):
     def test_item_matches_query_name(self):
         test = StoreItem.objects.get(item_id=1)
         self.assertEqual(str(test), 'test')
+
+
+class EcommerceApiTests(APITestCase):
+    def setUp(self):
+        StoreItem.objects.create(
+            item_name='test', item_price=3.99, item_thumb_url='https://example.com')
+
+    def test_get_items(self):
+        url = reverse('ecommerce:items')
+
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
