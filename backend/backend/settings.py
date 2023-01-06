d9caf1492b43064e2fc70dc71d49a61e36bd6444
NeptuneRjo <rjo9000@outlook.com>
2023-01-06 02:23:48 -0500

    add render to allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-ecommerce-awf4.onrender.com']
 
 
 # Application definition
