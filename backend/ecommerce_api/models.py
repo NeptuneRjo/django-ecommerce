841579dbfbbc0d57b6970f335af4547e8e0177f2
NeptuneRjo <rjo9000@outlook.com>
2023-02-05 19:32:58 -0500

    update item models, add view to add new items

--- backend/ecommerce_api/models.py
+++ backend/ecommerce_api/models.py
@@ -3,11 +3,26 @@ from django.db import models
 # Create your models here.
 
 
+# class StoreItem(models.Model):
+#     item_name = models.CharField(max_length=220)
+#     item_price = models.DecimalField(max_digits=8, decimal_places=2)
+#     item_thumb_url = models.CharField(max_length=180)
+# item_id = models.AutoField(primary_key=True)
+
+#     def __str__(self):
+#         return self.item_name
+
+
 class StoreItem(models.Model):
-    item_name = models.CharField(max_length=220)
-    item_price = models.DecimalField(max_digits=8, decimal_places=2)
-    item_thumb_url = models.CharField(max_length=180)
+    item_category = models.CharField(max_length=454, default='')
+    item_description = models.CharField(max_length=454, default='')
+    item_image_url = models.CharField(max_length=454, default='')
+    item_price = models.DecimalField(decimal_places=2, max_digits=8, default=0)
+    item_rating = models.DecimalField(
+        decimal_places=2, max_digits=8, default=0)
+    item_count = models.IntegerField(default=0)
+    item_title = models.CharField(max_length=454, default='')
     item_id = models.AutoField(primary_key=True)
 
     def __str__(self):
-        return self.item_name
+        return self.item_title
--- backend/ecommerce_api/urls.py
+++ backend/ecommerce_api/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'ecommerce'
 
 urlpatterns = [
-    path('items', views.get_items, name='items')
+    path('items', views.get_items, name='items'),
+    path('add-items', views.post_items, name='add-items')
 ]
