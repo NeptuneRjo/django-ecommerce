9496b992a8261d08925c7787a465404b0ef06b2f
NeptuneRjo <rjo9000@outlook.com>
2023-02-06 23:42:20 -0500

    update models:

--- backend/ecommerce_api/models.py
+++ backend/ecommerce_api/models.py
@@ -14,14 +14,14 @@ from django.db import models
 
 
 class StoreItem(models.Model):
-    item_category = models.CharField(max_length=454, default='')
-    item_description = models.CharField(max_length=454, default='')
-    item_image_url = models.CharField(max_length=454, default='')
-    item_price = models.DecimalField(decimal_places=2, max_digits=8, default=0)
+    item_category = models.CharField(max_length=999, default='')
+    item_description = models.CharField(max_length=999, default='')
+    item_image_url = models.CharField(max_length=999, default='')
+    item_price = models.DecimalField(decimal_places=4, max_digits=8, default=0)
     item_rating = models.DecimalField(
-        decimal_places=2, max_digits=8, default=0)
+        decimal_places=4, max_digits=8, default=0)
     item_count = models.IntegerField(default=0)
-    item_title = models.CharField(max_length=454, default='')
+    item_title = models.CharField(max_length=999, default='')
     item_id = models.AutoField(primary_key=True)
 
     def __str__(self):
