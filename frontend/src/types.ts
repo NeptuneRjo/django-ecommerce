2530d787c2741e408832e0a869d54710bab832e5
NeptuneRjo <rjo9000@outlook.com>
2023-02-16 03:12:28 -0500

    update storeitem interface

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,7 +1,12 @@
 export interface StoreItemInt {
-	item_name: string
-	item_price: number
-	item_thumb_url: string
+	item_category: string
+	item_description: string
+	item_image_url: string
+	item_price: string
+	item_rating: string
+	item_count: string
+	item_title: string
+	item_id: string
 }
 
 export interface AccountInt {
