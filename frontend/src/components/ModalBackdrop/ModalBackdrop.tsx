959d62019ed441d29dbecf6ea5062a8952d99ec7
NeptuneRjo <rjo9000@outlook.com>
2023-02-17 01:33:32 -0500

    rename to be less specific

---
+++ frontend/src/components/ModalBackdrop/ModalBackdrop.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import './styles.css'
+
+type Props = {
+	handleClose: () => void
+}
+
+const ModalBackdrop: React.FC<Props> = ({ handleClose }: Props) => {
+	return (
+		<motion.div
+			key='backdrop'
+			onClick={handleClose}
+			className='card-backdrop'
+			variants={{
+				hidden: {
+					opacity: 0,
+					transition: {
+						duration: 0.16,
+					},
+				},
+				visible: {
+					opacity: 0.8,
+					transition: {
+						delay: 0.04,
+						duration: 0.2,
+					},
+				},
+			}}
+			initial='hidden'
+			exit='hidden'
+			animate='visible'
+		/>
+	)
+}
+
+export default ModalBackdrop
